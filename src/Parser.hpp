e47efd8df27f210964e938c2dcb690dd31f533a7
cjr61 <cjr61@zips.uakron.edu>
2017-10-16 10:31:28 -0400

    Improved expectType error throwing

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -9,39 +9,41 @@ class Parser
 {
 public:
     Parser(std::vector<Token>& tokens_);
-    
+
     void parse();
-    
+
 private:
     Token& currentToken()
     {
         if(currentTokenId < (int)tokens.size())
             return tokens[currentTokenId];
-        
+
         return lastToken;
     }
-    
+
     void expectType(TokenType expectedType)
     {
         // TODO: name the tokens and print what was expected
         if(currentToken().type != expectedType)
-            throwErrorAtCurrentLocation("Unexpected token: " + currentToken().value);
+            throwErrorAtCurrentLocation(
+              "Unexpected token: " + currentToken().value
+              + " Expected: " + Token::getTokenName(expectedType)
+              );
     }
-                
+
     void nextToken() { ++currentTokenId; }
-    
+
     ExpressionNode* parseExpression();
     ExpressionNode* parseTerm();
     ExpressionNode* parseFactor();
-    
+
     bool parseVarDecl();
     void parseHeader();
-    
+
     void throwErrorAtCurrentLocation(std::string errorMessage);
-    
+
     std::vector<Token>& tokens;
     int currentTokenId;
     Token lastToken;
     Ast ast;
 };
-
